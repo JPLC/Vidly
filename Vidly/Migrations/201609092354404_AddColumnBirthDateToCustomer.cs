3a45bd9e52f9b44b261f74063ebeb1c754b7dd4a
João Caetano <joao_caetano10@hotmail.com>
2016-09-10 01:01:11 +0100

    Exercise 2 from the section 3 is completed

---
+++ Vidly/Migrations/201609092354404_AddColumnBirthDateToCustomer.cs
@@ -0,0 +1,17 @@
+namespace Vidly.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnBirthDateToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "BirthDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "BirthDate");
+        }
+    }
+}
