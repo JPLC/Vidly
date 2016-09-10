7f35e981b79cd477710e18b7791066a8daae8d9b
João Caetano <joao_caetano10@hotmail.com>
2016-09-10 01:33:11 +0100

    Almost Finished Exercise 3 from section 3 (must correct error on migrations)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,37 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController()
         {
-            var movies = GetMovies();
-            return View(movies);
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = _context.Movies;
+            return View(movies.ToList());
         }
 
         // GET: Movies/{Id}
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Wall-e"}
-            };
         }
     }
 }
